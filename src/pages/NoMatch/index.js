48c8c4bca81791c07df6a3d55965952d97a82287
Jae Kim <jaeshinkim321@gmail.com>
2020-12-21 21:55:00 -0800

    nomatch page css changes

--- src/pages/NoMatch/index.js
+++ src/pages/NoMatch/index.js
@@ -7,13 +7,14 @@ export default function NoMatch() {
             <div className="card"
              style={{
                 backgroundColor: "honeydew",
+                color: 'midnightblue',
                 border: "2px solid slateblue",
                 boxShadow: "5px 5px 5px mediumslateblue",
                 textAlign: "center"
             }}
             >
                 <div className="card-body">
-                    <h5 className="card-title">Error 404: No page found.</h5>
+                    <h4 className="card-title"><strong>Error 404: No page found.</strong></h4>
                     <img src="https://i.imgur.com/jMAUk7U.jpg?1" />
                 </div>
             </div>
